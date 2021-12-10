1b5e7a4031784927536eb9f99df03a044bdeca8c
Jelle van Polanen <jellevanpolanen2002@gmail.com>
2021-12-10 14:30:13 +0100

    sorteer functie toegevoegd

--- src/routes/movie-card.svelte
+++ src/routes/movie-card.svelte
@@ -2,25 +2,27 @@
 	export let movieObject;
 </script>
 
-<div class="card m-3 movie-card">
-	<img
+<a
+href={`/movie/${movieObject.id}-${movieObject.title.replaceAll(' ', '-')}`}
+		class="btn text-decoration-none">
+<div class="card m-3 movie-card shadow-lg rounded overflow-hidden">
+	
+	<img 
 		src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movieObject.poster_path}`}
-		class="card-img-top"
+		class="card-img-top "
 		alt="..."
 	/>
 	<div class="card-body">
 		<h5 class="card-title">{movieObject.title}</h5>
 		<p class="card-text">{movieObject.release_date}</p>
-		<a
-			href={`/movie/${movieObject.id}-${movieObject.title.replaceAll(' ', '-')}`}
-			class="btn btn-primary">Go somewhere</a
-		>
-	</div>
+			</div>
 </div>
+</a>
 
 <style>
 	.card.movie-card {
 		flex-basis: 15rem;
 		max-width: 15rem;
+		border : 0;
 	}
 </style>
--- src/routes/search-movies.svelte
+++ src/routes/search-movies.svelte
@@ -8,13 +8,43 @@
 		const body = await response.json();
 		movieArray = body.results;
 	}
+
+	function sortByPopular() {
+		movieArray = movieArray.sort(function (a, b) {
+			return b.popularity-a.popularity;
+		});
+	}
+	function sortByDate() {
+		movieArray = movieArray.sort(function (a, b) {
+			return b.release_date.localeCompare(a.release_date);
+		});
+	}
+	function sortByRating() {
+		movieArray = movieArray.sort(function (a, b) {
+			return b.vote_average-a.vote_average;
+		});
+	}
+	function sortByTitle() {
+		movieArray = movieArray.sort(function (a, b) {
+			return a.title.localeCompare(b.title);
+		});
+	}
 </script>
 
 <h1>Search movies</h1>
 
 <form on:submit|preventDefault={goSearch} class="search">
-	<input type='search' bind:value={searchedText} /><button type='submit'>search</button>
+	<input type="search" bind:value={searchedText} /><button type="submit">search</button>
 </form>
+
+{#if movieArray.length}
+<div class='mt-3'>
+	<button class='btn btn-light' on:click={sortByDate}>Datum</button>
+	<button class='btn btn-light' on:click={sortByPopular}>Populariteit</button>
+	<button class='btn btn-light' on:click={sortByRating}>Rating</button>
+	<button class='btn btn-light' on:click={sortByTitle}>Titel</button>
+</div>
+{/if}
 
 <div class="card-group">
 	{#each movieArray as movieObject}<MovieCard {movieObject} />{/each}
