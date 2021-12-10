cb2e036b60c86d36ccb499df80ae79aed51de9ca
Jelle van Polanen <jellevanpolanen2002@gmail.com>
2021-12-10 15:44:50 +0100

    plaatjes gefixt en 100 films per page

--- src/routes/movie-card.svelte
+++ src/routes/movie-card.svelte
@@ -1,28 +1,34 @@
 <script>
 	export let movieObject;
+	import witplaatje from './witplaatje.png';
 </script>
 
 <a
-href={`/movie/${movieObject.id}-${movieObject.title.replaceAll(' ', '-')}`}
-		class="btn text-decoration-none">
-<div class="card m-3 movie-card shadow-lg rounded overflow-hidden">
-	
-	<img 
-		src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movieObject.poster_path}`}
-		class="card-img-top "
-		alt="..."
-	/>
-	<div class="card-body">
-		<h5 class="card-title">{movieObject.title}</h5>
-		<p class="card-text">{movieObject.release_date}</p>
-			</div>
-</div>
+	href={`/movie/${movieObject.id}-${movieObject.title.replaceAll(' ', '-')}`}
+	class="btn text-decoration-none"
+>
+	<div class="card m-3 movie-card shadow-lg rounded overflow-hidden">
+		<img
+			src={movieObject.poster_path
+				? `https://www.themoviedb.org/t/p/w220_and_h330_face${movieObject.poster_path}`
+				: witplaatje}
+			class="card-img-top "
+			alt="..."
+			height="330"
+			width="220"
+			loading='lazy'
+		/>
+		<div class="card-body">
+			<h5 class="card-title">{movieObject.title}</h5>
+			<p class="card-text">{movieObject.release_date}</p>
+		</div>
+	</div>
 </a>
 
 <style>
 	.card.movie-card {
 		flex-basis: 15rem;
 		max-width: 15rem;
-		border : 0;
+		border: 0;
 	}
 </style>
--- src/routes/search-movies.json.js
+++ src/routes/search-movies.json.js
@@ -1,16 +1,22 @@
-export async function get({query}) {
-    const searchedText = query.get('searchedText');
+export async function get({ query }) {
+	const searchedText = query.get('searchedText');
+	const pages = [1, 2, 3, 4, 5];
 
-
+	const resultPromises = pages.map(async (page) => {
 		const response = await fetch(
-			`https://api.themoviedb.org/3/search/movie?query=${searchedText}&api_key=${
-				import.meta.env.VITE_API_KEY
-			}&language=en-US`
+			`https://api.themoviedb.org/3/search/movie?query=${searchedText}&api_key=${import.meta.env.VITE_API_KEY
+			}&language=en-US&page=${page}`
 		);
+		const data = await response.json();
+		return data.results;
+	});
 
-		
+	const results = await Promise.all(resultPromises);
+	const movies = results.flatMap((e) => e);
+
 	return {
-		body: 
-			await response.json()
-		};
+		body: {
+			results: movies
+		}
+	};
 }
